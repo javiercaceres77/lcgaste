8417c60801031ba392bc87c6d11785cd35412550
Javier Caceres <javier.caceres77@gmail.com>
2015-02-25 15:40:55 +0000

    Update test_times.php

--- bat/test_times.php
+++ bat/test_times.php
@@ -13,11 +13,21 @@ date_default_timezone_set($conf_timezone);
 sanitize_input();
 unset($_POST, $_GET);
 
+$mins = 200;
+
 $obj_date_time = new date_time('now');
 
 pa($obj_date_time, 'obj_date_time');
 
-$obj_plus_mins = $obj_date_time->plus_mins(200);
+$obj_plus_mins = $obj_date_time->plus_mins($mins);
+
+pa($obj_plus_mins, 'obj_plus_mins');
+
+$obj_my_time = new my_time('now');
+
+pa($obj_my_time, 'obj_my_time');
+
+$obj_plus_mins = $obj_my_time->plus_mins($mins);
 
 pa($obj_plus_mins, 'obj_plus_mins');
 
