aff80bb306efea8620e152eff59a3244f340715f
Javier Caceres <javier.caceres77@gmail.com>
2015-03-16 16:21:55 +0000

    Update config.php
    some setups

--- inc/config.php
+++ inc/config.php
@@ -4,17 +4,17 @@ $conf_include_path = 'inc/';
 $conf_images_path = 'img/';
 $conf_photos_path = 'pho/';
 $conf_main_page = 'index.php';
-$conf_main_url = 'http://www.lcgaste.com/pagerbook/';
+$conf_main_url = 'http://www.lcgaste.com/';
 $conf_logs_path = 'log/';
 $conf_mods_path = 'mod/';
 $conf_default_mod = 'home';
 
 // SETTINGS
-$conf_db_host = 'pagerbookuser.db.9132695.hostedresource.com';
+$conf_db_host = '127.0.0.1';
 $conf_db_type = 'mysql';
-$conf_db_name = 'pagerbookuser';
-$conf_db_user = 'dWh2eG5ycmV1aGpkcw==';
-$conf_db_pass = 'aGhvOjlve2xX';
+$conf_db_name = 'lcgaste';
+$conf_db_user = 'bHNrc2x5ZG0=';
+$conf_db_pass = 'QHJkb09kN2RF';
 $conf_db_date_format = 'Y-m-d';
 $conf_db_datetime_format = 'Y-m-d H:i:s';
 
@@ -24,9 +24,5 @@ $conf_generic_user_id = 0;
 
 // LOCAL & LANGUAGES
 $conf_default_lang = 'es';
-$conf_timezone = 'Europe/Madrid';
-
-// SC variables
-//$conf_main_phone_contact = '987 406 060 / 636 152 765';
-
+$conf_timezone = 'Europe/London';
 ?>
