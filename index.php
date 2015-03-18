f7ac313f8664e01df0ffe934590f5c5e1ae6fbca
Javier Caceres <javier.caceres77@gmail.com>
2015-03-18 15:12:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -200,7 +200,6 @@ getData3(url, 'alerts_box');
 
 
 
-
 <div id="content">
 <div id="alerts_wrapper">
 <div id="alerts" class="notice_info">
@@ -227,8 +226,60 @@ include $include_file;
 <div id="footer">
 <p>footer info here </p>
 </div>
-
-<p>&nbsp;</p>
+<!-- Grab Google CDN's jQuery, fall back to local if offline -->
+<!-- 2.0 for modern browsers, 1.10 for .oldie -->
+<script>
+var oldieCheck = Boolean(document.getElementsByTagName('html')[0].className.match(/\soldie\s/g));
+if(!oldieCheck) {
+document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"><\/script>');
+} else {
+document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"><\/script>');
+}
+</script>
+<script>
+if(!window.jQuery) {
+if(!oldieCheck) {
+document.write('<script src="inc/js/libs/jquery-2.0.2.min.js"><\/script>');
+} else {
+document.write('<script src="inc/js/libs/jquery-1.10.1.min.js"><\/script>');
+}
+}
+</script>
+<!--
+Include gumby.js followed by UI modules followed by gumby.init.js
+Or concatenate and minify into a single file -->
+<script gumby-touch="inc/js/libs" src="inc/js/libs/gumby.js"></script>
+<script src="inc/js/libs/ui/gumby.retina.js"></script>
+<script src="inc/js/libs/ui/gumby.fixed.js"></script>
+<script src="inc/js/libs/ui/gumby.skiplink.js"></script>
+<script src="inc/js/libs/ui/gumby.toggleswitch.js"></script>
+<script src="inc/js/libs/ui/gumby.checkbox.js"></script>
+<script src="inc/js/libs/ui/gumby.radiobtn.js"></script>
+<script src="inc/js/libs/ui/gumby.tabs.js"></script>
+<script src="inc/js/libs/ui/gumby.navbar.js"></script>
+<script src="inc/js/libs/ui/jquery.validation.js"></script>
+<script src="inc/inc/js/libs/gumby.init.js"></script>
+<!--
+Google's recommended deferred loading of JS
+gumby.min.js contains gumby.js, all UI modules and gumby.init.js
+Note: If you opt to use this method of defered loading,
+ensure that any javascript essential to the initial
+display of the page is included separately in a normal
+script tag.
+<script type="text/javascript">
+function downloadJSAtOnload() {
+var element = document.createElement("script");
+element.src = "js/libs/gumby.min.js";
+document.body.appendChild(element);
+}
+if (window.addEventListener)
+window.addEventListener("load", downloadJSAtOnload, false);
+else if (window.attachEvent)
+window.attachEvent("onload", downloadJSAtOnload);
+else window.onload = downloadJSAtOnload;
+</script> -->
+<script src="inc/js/plugins.js"></script>
+<script src="inc/js/main.js"></script>
 </body>
 </html>
 <?php
@@ -330,59 +381,5 @@ include $include_file;
 <br /></td>
 </tr>
 </table>
-<!-- Grab Google CDN's jQuery, fall back to local if offline -->
-<!-- 2.0 for modern browsers, 1.10 for .oldie -->
-<script>
-var oldieCheck = Boolean(document.getElementsByTagName('html')[0].className.match(/\soldie\s/g));
-if(!oldieCheck) {
-document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"><\/script>');
-} else {
-document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"><\/script>');
-}
-</script>
-<script>
-if(!window.jQuery) {
-if(!oldieCheck) {
-document.write('<script src="inc/js/libs/jquery-2.0.2.min.js"><\/script>');
-} else {
-document.write('<script src="inc/js/libs/jquery-1.10.1.min.js"><\/script>');
-}
-}
-</script>
-<!--
-Include gumby.js followed by UI modules followed by gumby.init.js
-Or concatenate and minify into a single file -->
-<script gumby-touch="inc/js/libs" src="inc/js/libs/gumby.js"></script>
-<script src="inc/js/libs/ui/gumby.retina.js"></script>
-<script src="inc/js/libs/ui/gumby.fixed.js"></script>
-<script src="inc/js/libs/ui/gumby.skiplink.js"></script>
-<script src="inc/js/libs/ui/gumby.toggleswitch.js"></script>
-<script src="inc/js/libs/ui/gumby.checkbox.js"></script>
-<script src="inc/js/libs/ui/gumby.radiobtn.js"></script>
-<script src="inc/js/libs/ui/gumby.tabs.js"></script>
-<script src="inc/js/libs/ui/gumby.navbar.js"></script>
-<script src="inc/js/libs/ui/jquery.validation.js"></script>
-<script src="inc/inc/js/libs/gumby.init.js"></script>
-<!--
-Google's recommended deferred loading of JS
-gumby.min.js contains gumby.js, all UI modules and gumby.init.js
-Note: If you opt to use this method of defered loading,
-ensure that any javascript essential to the initial
-display of the page is included separately in a normal
-script tag.
-<script type="text/javascript">
-function downloadJSAtOnload() {
-var element = document.createElement("script");
-element.src = "js/libs/gumby.min.js";
-document.body.appendChild(element);
-}
-if (window.addEventListener)
-window.addEventListener("load", downloadJSAtOnload, false);
-else if (window.attachEvent)
-window.attachEvent("onload", downloadJSAtOnload);
-else window.onload = downloadJSAtOnload;
-</script> -->
-<script src="inc/js/plugins.js"></script>
-<script src="inc/js/main.js"></script>
 </body>
 </html>
