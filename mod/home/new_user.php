d1486275aa7398239ce48d11cc0fbc42b6804d5d
Javier Caceres <javier.caceres77@gmail.com>
2015-03-18 11:48:06 +0000

    Update new_user.php
    trying to adjust the height of the country selector

--- mod/home/new_user.php
+++ mod/home/new_user.php
@@ -10,7 +10,7 @@
 	background-color: #fff;
 	width: 218px;
 	border: 1px solid #B0BECA;
-	max-height: 350px;
+	max-height: 300px;
 	overflow-y: scroll;
 }
 .ui-autocomplete .ui-menu-item a {
@@ -188,7 +188,7 @@ if($ob_user->user_id == $conf_generic_user_id) {
 		$arr_countries = dump_table2($ob_table->get_translated_table_name(), 'country_id', $arr_fields, ' WHERE active_ind = \'1\'');
 //		pa($arr_countries);
 		?>
-          <select name="country" class="input_normal"  id="country-selector" style="width: 250px;" onblur="JavaScript:check_value('country','');" />
+          <select name="country" class="input_normal"  id="country-selector" style="width: 250px; height: 300px;" onblur="JavaScript:check_value('country','');" />
           <?php
 		  if(!$_POST['country']) $_POST['country'] = 'ES';
 		  foreach($arr_countries as $country) {
