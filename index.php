ff18fce60f78e2ff8e7d434aa0a60b497f647425
Javier Caceres <javier.caceres77@gmail.com>
2015-03-19 12:01:22 +0000

    Update index.php

--- index.php
+++ index.php
@@ -100,10 +100,10 @@ if(!$_GET['mod']) $_GET['mod'] = $conf_default_mod;
 				<a class="toggle" gumby-trigger="#nav3 > .row > ul" href="#"><i class="icon-menu"></i></a>
 				<h3 class="six columns"><a href="<?= $conf_main_page; ?>">www.lcgaste.com</a></h3>
 				<ul class="six columns">
-					<li class="append field valign row"><input class="normal email input" placeholder="Email" type="email" id="user">
+					<li class="append field"><input class="normal email input" placeholder="Email" type="email" id="user">
 							<span class="adjoined">@</span></li>
-					<li class="field valign row"><input class="normal password input" placeholder="Passw" type="password" id="pass"></li>
-					<li class="pretty medium info btn valign row"><button><i class="icon-lock-open"></i></button></li>
+					<li class="field"><input class="normal password input" placeholder="Passw" type="password" id="pass"></li>
+					<li class="pretty medium info btn"><button><i class="icon-lock-open"></i></button></li>
 				</ul>
 				</div>
 			</div>
