62a19fe46711a17b94827ee12c98f9ba6febf074
Javier Caceres <javier.caceres77@gmail.com>
2015-04-02 15:17:22 +0100

    Update monitory.py

--- bat/monitory.py
+++ bat/monitory.py
@@ -5,3 +5,8 @@ cc128 = serial.Serial("/dev/ttyUSB0", 57600, timeout=6)
 cc128xml = cc128.readlines(6)
 print cc128xml
 
+now = datetime.datetime.now()
+print now
+
+nowgmt = time.gmtime()
+print nowgmt
