4116a9a9ea41a09d65e0e5cdfa0edf1c38b03ef7
Javier Caceres <javier.caceres77@gmail.com>
2015-04-02 15:36:52 +0100

    Update monitory.py

--- bat/monitory.py
+++ bat/monitory.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 
-import serial
 import datetime
 import time
 
-cc128 = serial.Serial("/dev/ttyUSB0", 57600, timeout=6)
-cc128xml = cc128.readlines(6)
-print cc128xml
-
-now = datetime.datetime.now()
-print datetime.datetime.dst()
+now = datetime.datetime.dst()
+print now.strftime("%H")
 
 nowgmt = time.localtime()
 print nowgmt
