cf5af77ec57958c2e1d96725889b97d6a1d344fb
Javier Caceres <javier.caceres77@gmail.com>
2015-05-01 11:10:32 +0100

    Update xml_prc2.php

--- bat/xml_prc2.php
+++ bat/xml_prc2.php
@@ -53,7 +53,7 @@ foreach($arr_directory as $file_name) {
 				# check that it is the same hour as some lines could be included in incorrect files.
 				if($obj_line_time->hour == $obj_file_datetime->hour)
 					if($objxml->hist) {			# this is a history line
-						if($objxml->hist->data[0]->d090)
+						if($objxml->hist->data[0]->d030)
 							pa($objxml);
 						continue;
 					}
