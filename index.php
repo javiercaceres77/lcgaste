488972c35aa2ec91606ce6e445389cbb1b452a25
Javier Caceres <javier.caceres77@gmail.com>
2015-05-07 16:49:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ if(!$_GET['mod']) $_GET['mod'] = $conf_default_mod;
     <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1><span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>Hello, world!</h1>
+        <h1>Hello, world!</h1>
         <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
       </div>
